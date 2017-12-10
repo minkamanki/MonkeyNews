c6c548b46d037006fc2c611594e8df34106ab211
minkamanki <minka.tahvanainen@gmail.com>
2017-12-10 17:13:37 +0200

    Sama nyt myös gategorioille

--- src/main/java/uutiset/controller/CategoryController.java
+++ src/main/java/uutiset/controller/CategoryController.java
@@ -7,36 +7,42 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import uutiset.domain.Category;
+import uutiset.service.ArticleService;
 import uutiset.service.CategoryService;
 
 @Controller
 public class CategoryController {
+
     @Autowired
     private CategoryService categoryService;
- 
+    @Autowired
+    private ArticleService articleService;
+
     @RequestMapping(value = "/categories", method = RequestMethod.GET)
     public String list(Model model) {
         model.addAttribute("categories", categoryService.list());
         return "categories";
     }
- 
+
     @RequestMapping(value = "/categories", method = RequestMethod.POST)
-    public String add(@RequestParam String name, @RequestParam(required=false) String nav) {
+    public String add(@RequestParam String name, @RequestParam(required = false) String nav) {
         categoryService.add(name, nav);
         return "redirect:/categories";
     }
- 
+
     @RequestMapping(value = "/categories/{categoryId}", method = RequestMethod.GET)
     public String view(Model model, @PathVariable(value = "categoryId") Long categoryId) {
-        model.addAttribute("category", categoryService.findById(categoryId));
+        Category category = categoryService.findById(categoryId);
+        model.addAttribute("category", category);
+        model.addAttribute("articles", articleService.findByCategory(category.getName()));
         return "category";
     }
- 
+
     @RequestMapping(value = "/categories/{categoryId}", method = RequestMethod.DELETE)
     public String remove(@PathVariable(value = "categoryId") Long categoryId) {
         categoryService.remove(categoryId);
         return "redirect:/categories";
     }
- 
 
 }
